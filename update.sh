92a3c4728d7e85a56d3a76ef15efe73ffa8912c8
ningmengchongshui <916415899@qq.com>
2024-06-11 21:35:14 +0800

    docs: 修改文档

--- update.sh
+++ update.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
-
 git init
-git add -A
-git commit -m 'add'
-
+git add .
+git commit -m 'docs: 修改文档'
 git push -f git@github.com:ningmengchongshui/Miao-Yunzai-Docs.git master:main
