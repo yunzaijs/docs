26a902061ff925ab21fb6d924fc1654d508a67f1
ningmengchongshui <916415899@qq.com>
2024-06-18 15:51:11 +0800

    Add GitHub Actions workflow for packaging

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
 set -e
-
-# 生成静态文件
-
+npm install
 npm run build
-
-# 进入生成的文件夹
-
 cd build/
-
 git init
 git add -A
 git commit -m 'add'
-
 git push -f git@github.com:ningmengchongshui/Miao-Yunzai-Docs.git master:docs
